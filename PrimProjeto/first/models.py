1ff07a587f863d21cf852e837a8af449d0a35e42
Livia <livialutz385@gmail.com>
2022-09-25 17:44:51 -0300

    Criei as classes de acordo com a modelagem mas ainda não está pronto

--- PrimProjeto/first/models.py
+++ PrimProjeto/first/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class usuario:
+    nome=
+    sobrenome=
+    email=
+class disciplina:
+    codigo=
+    professores=
+    materiais=
+class mensagem:
+    data=
+    hora=
+class avaliacao:
+   professor=
+   satisfacaoProfessor=
+   cobranca=
+   horasFora=
+   contribuicao=
+   dificuldade=
+   observacao=
+class pergunta:
+    data=
+    hora=
+    conteudo=
+class resposta:
+    data=
+    hora=
+    conteudo=
+class encontro:
+    local=
+    horaio=
+    dia=
+    pessoasConfirmadas=
+    frequincia=
+class demanda:
+    frequencia=
+
+
+
