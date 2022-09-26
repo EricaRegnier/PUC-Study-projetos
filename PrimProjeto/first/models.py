6177a8614854d3f093b81040b18d17ca7c32968a
Livia <108961867+livialutz14@users.noreply.github.com>
2022-09-26 09:45:04 -0300

    Update models.py
    Terminei de classificar os atributos :) ainda não sei se tá tudo certo ou se tem algo faltando, portanto ainda está sob revisão

--- PrimProjeto/first/models.py
+++ PrimProjeto/first/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 #tudo que tá vazio é pq eu não sei em qual classificação o atributo se encaixa
 
 class usuario(models.Model):
-    nome=models.CharField(max_length=100)
-    sobrenome=models.CharField(max_length=100)
+    nome=models.CharField(max_length=30)
+    sobrenome=models.CharField(max_length=30)
     email=models.EmailField()
     def _str_(self):
         return self.nome
@@ -19,36 +20,36 @@ class disciplina:
 
 class mensagem:
     data=models.DateField()
-    hora=
+    hora=models.CharField(max_length=5)
 
 class avaliacao:
    professor=models.CharField(max_length=100)
-   satisfacaoProfessor=models.IntegerField(default=2)
-   cobranca=
+   satisfacaoProfessor=models.CharField(max_length=7)
+   cobranca=models.IntegerField(default=2) #pode ser 01,02..10
    horasFora=models.IntegerField()
-   contribuicao=
-   dificuldade=
-   observacao=
+   contribuicao=models.IntegerField(deffault=2)#mesma coisa da cobrança
+   dificuldade=models.IntegerField(deffault=2)
+   observacao=models.TextField()
 
 class pergunta:
     data=models.DateField()
-    hora=
+    hora=models.CharField(max_length=5)
     conteudo=models.TextField()
 
 class resposta:
     data=models.DateField()
-    hora=
+    hora=models.CharField(max_length=5)
     conteudo=models.TextField()
 
 class encontro:
-    local=
-    horaio=
+    local=models.CharField(max_length=100)
+    horaio=models.CharField(max_length=5)
     dia=models.DateField()
     pessoasConfirmadas=models.ManyToManyField()
-    frequincia=
+    frequincia=models.CharField(max_length=100)
 
 class demanda:
-    frequencia=
+    frequencia=models.CharField(max_length=100)
 
 
 
