7fa668493d86ee755d8574840ad906f38c5f447e
Livia <108961867+livialutz14@users.noreply.github.com>
2022-09-28 10:49:54 -0300

    Me referindo às classes com letra maiúscula

--- PrimProjeto/first/admin.py
+++ PrimProjeto/first/admin.py
@@ -1,82 +1,82 @@
 from django.contrib import admin
-from .models import usuario
-from .models import disciplina
-from .models import mensagem
-from .models import avaliacao
-from .models import pergunta
-from .models import resposta
-from .models import encontro
-from .models import demanda
+from .models import Usuario
+from .models import Disciplina
+from .models import Mensagem
+from .models import Avaliacao
+from .models import Pergunta
+from .models import Resposta
+from .models import Encontro
+from .models import Demanda
 
 
 # Register your models here.
 
-from first.models import usuario
-n=usuario(nome=input("Digite seu primiro nome: "))
+from first.models import Usuario
+n=Usuario(nome=input("Digite seu primiro nome: "))
 n.save()
-s=usuario(sobrenome=input("Digite seu sobrenome: "))
+s=Usuario(sobrenome=input("Digite seu sobrenome: "))
 s.save()
-e=usuario(email=input("Digite seu e-mail: "))
+e=Usuario(email=input("Digite seu e-mail: "))
 e.save()
 
-from first.models import disciplina
-c=disciplina(codigo=input("Digite o codigo da dsiciplina: "))
+from first.models import Disciplina
+c=Disciplina(codigo=input("Digite o codigo da dsiciplina: "))
 c.save()
-p=disciplina(professores=input("Digite o nome dos professores: "))
+p=Disciplina(professores=input("Digite o nome dos professores: "))
 p.save()
-m=disciplina(materiais=input("Materiais usados: "))
+m=Disciplina(materiais=input("Materiais usados: "))
 m.save()
 
-from first.models import mensagem
-d=mensagem(data=input("Data da mensagem: "))
+from first.models import Mensagem
+d=Mensagem(data=input("Data da mensagem: "))
 d.save()
-h=mensagem(hora=input("hora da mensagem: "))
+h=Mensagem(hora=input("hora da mensagem: "))
 h.save()
 
-from first.models import avaliacao
-p=avaliacao(professor=input("Professor: "))
+from first.models import Avaliacao
+p=Avaliacao(professor=input("Professor: "))
 p.save()
-s=avaliacao(satisfacaoProfessor=input(" Numa escala de 0 a 10 qual foi a sua Satisfacao com o Professor? "))
+s=Avaliacao(satisfacaoProfessor=input(" Numa escala de 0 a 10 qual foi a sua Satisfacao com o Professor? "))
 s.save()
-c=avaliacao(cobranca=input("Numa escala de 0 a 10 qual o nivel de Cobranca da dsiciplina? "))
+c=Avaliacao(cobranca=input("Numa escala de 0 a 10 qual o nivel de Cobranca da dsiciplina? "))
 c.save()
-h=avaliacao(horasFora=input("Horas que voce passou estudando fora do horario de aula: "))
+h=Avaliacao(horasFora=input("Horas que voce passou estudando fora do horario de aula: "))
 h.save()
-cont=avaliacao(contribuicao=input("Numa escala de 0 a 10,quanto a disciplina contribuiu para o seu progesso no curso? "))
+cont=Avaliacao(contribuicao=input("Numa escala de 0 a 10,quanto a disciplina contribuiu para o seu progesso no curso? "))
 cont.save()
-d=avaliacao(dificuldade=input("Numa escala de 0 a 10 qual e a dificuldade da disciplina: "))
+d=Avaliacao(dificuldade=input("Numa escala de 0 a 10 qual e a dificuldade da disciplina: "))
 d.save()
-o=avaliacao(observacao=input("Observacoes sobre a disciplina: "))
+o=Avaliacao(observacao=input("Observacoes sobre a disciplina: "))
 o.save()
-from first.models import pergunta
-d=pergunta(data=input("Data da pergunta: "))
+from first.models import Pergunta
+d=Pergunta(data=input("Data da pergunta: "))
 d.save()
-h=pergunta(hora=input("Hora da pergunta: "))
+h=Pergunta(hora=input("Hora da pergunta: "))
 h.save()
-c=pergunta(conteudo=input("Digite sua pergunta: "))
+c=Pergunta(conteudo=input("Digite sua pergunta: "))
 c.save()
 
-from first.models import resposta
-d=resposta(data=input("Data da resposta: "))
+from first.models import Resposta
+d=Resposta(data=input("Data da resposta: "))
 d.save()
-h=resposta(hora=input("Hora da resposta: "))
+h=Resposta(hora=input("Hora da resposta: "))
 h.save()
-c=resposta(conteudo=input("Digite sua resposta: "))
+c=Resposta(conteudo=input("Digite sua resposta: "))
 c.save()
 
-from first.models import encontro
-l=encontro(local=input("Digite o local de encontro: "))
+from first.models import Encontro
+l=Encontro(local=input("Digite o local de encontro: "))
 l.save()
-h=encontro(horario=input("Digite o horario de encontro: "))
+h=Encontro(horario=input("Digite o horario de encontro: "))
 h.save()
-d=encontro(dia=input("Digite o dia de encontro: "))
+d=Encontro(dia=input("Digite o dia de encontro: "))
 d.save()
-p=encontro(pessoasConfirmadas=input("Pessoas confirmadas no encontro: "))
+p=Encontro(pessoasConfirmadas=input("Pessoas confirmadas no encontro: "))
 p.save()
-f=encontro(frequincia=input("Digite a frequencia de encontro: "))
+f=Encontro(frequincia=input("Digite a frequencia de encontro: "))
 f.save()
 
 
-from first.models import demanda
-f=demanda(frequencia=input("Frequencia de atividades: "))
+from first.models import Demanda
+f=Demanda(frequencia=input("Frequencia de atividades: "))
 
