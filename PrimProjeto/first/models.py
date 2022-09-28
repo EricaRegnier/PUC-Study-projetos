703d567a4d55b4d738a7821ede1774c2177fbfd8
Livia <108961867+livialutz14@users.noreply.github.com>
2022-09-28 10:46:02 -0300

    Nome das classes começando com letra maiúscula

--- PrimProjeto/first/models.py
+++ PrimProjeto/first/models.py
@@ -5,7 +5,7 @@ from datetime import date
 
 #tudo que tá vazio é pq eu não sei em qual classificação o atributo se encaixa
 
-class usuario(models.Model):
+class Usuario(models.Model):
     nome=models.CharField(max_length=30)
     sobrenome=models.CharField(max_length=30)
     email=models.EmailField()
@@ -13,16 +13,16 @@ class usuario(models.Model):
         return self.nome
 
 
-class disciplina:
+class Disciplina:
     codigo=models.IntegerField(default=7)
     professores=models.TextField()
     materiais=models.TextField()
 
-class mensagem:
+class Mensagem:
     data=models.DateField()
     hora=models.CharField(max_length=5)
 
-class avaliacao:
+class Avaliacao:
    professor=models.CharField(max_length=100)
    satisfacaoProfessor=models.CharField(max_length=7)
    cobranca=models.IntegerField(default=2) #pode ser 01,02..10
@@ -31,24 +31,24 @@ class avaliacao:
    dificuldade=models.IntegerField(deffault=2)
    observacao=models.TextField()
 
-class pergunta:
+class Pergunta:
     data=models.DateField()
     hora=models.CharField(max_length=5)
     conteudo=models.TextField()
 
-class resposta:
+class Resposta:
     data=models.DateField()
     hora=models.CharField(max_length=5)
     conteudo=models.TextField()
 
-class encontro:
+class Encontro:
     local=models.CharField(max_length=100)
     horaio=models.CharField(max_length=5)
     dia=models.DateField()
     pessoasConfirmadas=models.ManyToManyField()
     frequincia=models.CharField(max_length=100)
 
-class demanda:
+class Demanda:
     frequencia=models.CharField(max_length=100)
 
 
