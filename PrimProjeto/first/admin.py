666253b891ec91b49c4d65fa089b0a9bf9372de6
Livia <108961867+livialutz14@users.noreply.github.com>
2022-09-27 22:56:15 -0300

    Modelagem inteiramente passada

--- PrimProjeto/first/admin.py
+++ PrimProjeto/first/admin.py
@@ -11,6 +11,49 @@ from .models import demanda
 
 # Register your models here.
 
-from PrimProjeto.models import usuario
-u=usuario(nome=Aninha)
-u.save()
+class usuario(models.Model):
+    nome=models.CharField(max_length=30)
+    sobrenome=models.CharField(max_length=30)
+    email=models.EmailField()
+    def _str_(self):
+        return self.nome
+
+
+class disciplina(models.Model):
+    codigo=models.IntegerField(default=7)
+    professores=models.TextField()
+    materiais=models.TextField()
+
+class mensagem(models.Model):
+    data=models.DateField()
+    hora=models.CharField(max_length=5)
+
+class avaliacao(models.Model):
+   professor=models.CharField(max_length=100)
+   satisfacaoProfessor=models.CharField(max_length=7)
+   cobranca=models.IntegerField(default=2) #pode ser 01,02..10
+   horasFora=models.IntegerField()
+   contribuicao=models.IntegerField(deffault=2)#mesma coisa da cobrança
+   dificuldade=models.IntegerField(deffault=2)
+   observacao=models.TextField()
+
+class pergunta(models.Model):
+    data=models.DateField()
+    hora=models.CharField(max_length=5)
+    conteudo=models.TextField()
+
+class resposta(models.Model):
+    data=models.DateField()
+    hora=models.CharField(max_length=5)
+    conteudo=models.TextField()
+
+class encontro(models.Model):
+    local=models.CharField(max_length=100)
+    horaio=models.CharField(max_length=5)
+    dia=models.DateField()
+    pessoasConfirmadas=models.TextField()
+    frequincia=models.CharField(max_length=100)
+
+class demanda(models.Model):
+    frequencia=models.CharField(max_length=100)
+
