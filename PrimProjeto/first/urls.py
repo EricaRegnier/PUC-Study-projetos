b83fadd1ae376d4f5545de7f7235ef494ae8cbb0
Livia <108961867+livialutz14@users.noreply.github.com>
2022-10-19 10:36:01 -0300

    Configurações do favicon

--- PrimProjeto/first/urls.py
+++ PrimProjeto/first/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
      path('', views.index, name='index'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('login', views.loginUsuario, name='login'),
     path('menu', views.menu, name='menu'),
     path('registroAvaliacao', views.registroAvaliacao, name='registroAvaliacao'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
